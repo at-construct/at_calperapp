21980fe7b133cad6ec0c9ac5ef92021a6bc6228e
[at81plus] <53667957+at-construct@users.noreply.github.com>
2023-06-06 06:45:12 +0900

    test

--- db/migrate/20230516101041_add_user_id_and_user_name_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdAndUserNameToEvents < ActiveRecord::Migration[6.1]
-  def change
-    add_column :events, :user_name, :string
-  end
-end
--- test/models/calendar_user_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CalendarUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
