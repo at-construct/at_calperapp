63701cbb46275d5db4e02a84576a55578867195f
[at81plus] <53667957+at-construct@users.noreply.github.com>
2023-06-18 21:53:30 +0900

    test

--- app/javascript/user_toggle.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var toggle = document.querySelector('.user-toggle');
-  toggle.addEventListener('click', function() {
-    this.classList.toggle('active');
-  });
-});
--- test/models/user_visibility_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class UserVisibilityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
